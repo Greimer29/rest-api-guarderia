54db085327a9bad2f5fbac5d3ede7407ffe368b3
greimer29 <greimerabelguzman29@gmail.com>
2023-12-05 17:14:59 -0400

    Ready RespFinanForm

--- guarderiapp/src/components/FormRespFinan.vue
+++ guarderiapp/src/components/FormRespFinan.vue
@@ -1,43 +1,43 @@
 <template>
   <div>
     <div class="q-gutter-xs q-my-sm">
-      <q-input v-model="father.firstLastName" filled label="Primer Apellido" />
-      <q-input v-model="father.secondLastName" filled label="Segundo Apellido" />
-      <q-input v-model="father.firstName" filled label="Primer Nombre" />
-      <q-input v-model="father.secondName" filled label="Segundo Nombre" />
-      <q-input v-model="father.age" filled type="number" label="Edad" />
+      <q-input v-model="respFinan.firstLastName" filled label="Primer Apellido" />
+      <q-input v-model="respFinan.secondLastName" filled label="Segundo Apellido" />
+      <q-input v-model="respFinan.firstName" filled label="Primer Nombre" />
+      <q-input v-model="respFinan.secondName" filled label="Segundo Nombre" />
+      <q-input v-model="respFinan.age" filled type="number" label="Edad" />
     </div>
     <div class="flex">
       <div class="q-pa-xs">
         <div class="text-bold">Nacionalidad</div>
-        <q-radio v-model="father.nacionality" val="V" label="Venezolano" />
-        <q-radio v-model="father.nacionality" val="E" label="Extranjero" />
+        <q-radio v-model="respFinan.nacionality" val="V" label="Venezolano" />
+        <q-radio v-model="respFinan.nacionality" val="E" label="Extranjero" />
       </div>
     </div>
     <div class="q-gutter-xs">
-      <q-input v-model="father.ci" filled label="Cedula de Identidad" />
-      <q-input v-model="father.dirHab" filled label="Direccion de habitacion" />
-      <q-input v-model="father.phone" filled label="Nro de Telefono" />
+      <q-input v-model="respFinan.ci" filled label="Cedula de Identidad" />
+      <q-input v-model="respFinan.dirHab" filled label="Direccion de habitacion" />
+      <q-input v-model="respFinan.phone" filled label="Nro de Telefono" />
     </div>
     <div class="q-my-sm q-gutter-xs">
       <div class="text-bold">Nivel Academinco</div>
       <div class="">
-        <q-checkbox left-label v-model="father.acadLevel" val="primaria" label="Priamria"  />
-        <q-checkbox left-label v-model="father.acadLevel" val="secundaria" label="Secundaria"/>
-        <q-checkbox left-label v-model="father.acadLevel" val="TSu" label="TSU" />
-        <q-checkbox left-label v-model="father.acadLevel" val="Lic" label="Lic" />
-        <q-checkbox left-label v-model="father.acadLevel" val="MA" label="MA" />
-        <q-checkbox left-label v-model="father.acadLevel" val="PhD" label="PhD" />
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="primaria" label="Priamria"  />
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="secundaria" label="Secundaria"/>
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="TSu" label="TSU" />
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="Lic" label="Lic" />
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="MA" label="MA" />
+        <q-checkbox left-label v-model="respFinan.acadLevel" val="PhD" label="PhD" />
       </div>
     </div>
     <div class="q-gutter-xs ">
-        <q-select v-model="father.labSituation" :options="situatioOp" filled label="Situacion Laboral" />
-        <q-input v-model="father.profesion" filled label="Profesion" />
-        <q-input v-model="father.actJob" filled label="Ocupacion Actual" />
-        <q-input v-model="father.jobPlace" filled label="Lugar de Trabajo" />
-        <q-input v-model="father.jobPhone" filled label="Telefonos de Trabajo" />
+        <q-select v-model="respFinan.labSituation" :options="situatioOp" filled label="Situacion Laboral" />
+        <q-input v-model="respFinan.profesion" filled label="Profesion" />
+        <q-input v-model="respFinan.actJob" filled label="Ocupacion Actual" />
+        <q-input v-model="respFinan.jobPlace" filled label="Lugar de Trabajo" />
+        <q-input v-model="respFinan.jobPhone" filled label="Telefonos de Trabajo" />
     </div>
-    <q-btn label="enviar" @click="enviar(father)"/>
+    <q-btn label="enviar" @click="enviar(respFinan)"/>
   </div>
 </template>
 
@@ -49,7 +49,7 @@ export default defineComponent({
   setup(){
     const situatioOp = ref(['Desempleado','Emprendedor','Mama Luchona'])
     const civilOp = ref(['Casado','Divorciado','Viudo','Soltero'])
-    const father = ref({
+    const respFinan = ref({
       firstLastName : '',
       secondLastName : '',
       firstName : '',
@@ -57,7 +57,6 @@ export default defineComponent({
       age : '',
       nacionality : '',
       ci : '',
-      civilState : '',
       dirHab : '',
       phone : '',
       acadLevel:[],
@@ -68,12 +67,12 @@ export default defineComponent({
       jobPhone : ''
     })
 
-    const enviar = (father) => {
-      console.log(father)
+    const enviar = (respFinan) => {
+      console.log(respFinan)
     }
 
     return{
-      father,
+      respFinan,
       enviar,
       situatioOp,
       civilOp
