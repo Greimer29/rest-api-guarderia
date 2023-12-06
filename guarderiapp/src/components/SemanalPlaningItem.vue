a2dd9f157aa653719401c189f108fa5d5776670a
greimer29 <greimerabelguzman29@gmail.com>
2023-12-06 16:04:32 -0400

    Listo el componente de planificacion semanal

--- guarderiapp/src/components/SemanalPlaningItem.vue
+++ guarderiapp/src/components/SemanalPlaningItem.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="q-pa-xs col-xs-12 col-sm-6 col-md-4">
     <q-card flat bordered>
-      <q-card-section class="text-center q-pb-none q-gutter-sm">
+      <q-card-section class="text-center q-pt-xl q-gutter-sm">
         <strong>{{ datos.actividad }}</strong>
         <q-separator />
         <div>{{ datos.dia }}</div>
       </q-card-section>
       <q-card-section class="full-width q-pa-xs text-right q-gutter-xs">
-        <q-btn icon="edit" color="primary" dense @click="actActividad = true"/>
+        <q-btn icon="edit" color="primary" dense @click="open"/>
         <q-btn icon="delete" color="negative" dense @click="borrar(datos.id)"/>
       </q-card-section>
     </q-card>
@@ -20,10 +20,10 @@
         <q-btn flat round dense icon="close" v-close-popup />
       </q-toolbar>
       <q-card-section class="q-gutter-md">
-        <q-select readonly filled v-model="dayOld" label="Dia" />
-        <q-input filled label="Actividad" type="textarea" v-model="activityOld"/>
+        <q-select filled v-model="dia" :options="dayOp" label="Dia" />
+        <q-input filled label="Actividad" type="textarea" v-model="actividad"/>
         <div class="full-width text-center">
-          <q-btn label="enviar" side color="positive" @click="actualizar(datos.id)" />
+          <q-btn label="Actualizar" side color="positive" @click="actualizar(datos.id,dia,actividad)" />
         </div>
       </q-card-section>
     </q-card>
@@ -34,35 +34,42 @@
 import { api } from 'src/boot/axios';
 import { defineComponent,ref } from 'vue';
 export default defineComponent({
+  emits:['reload'],
   name:'SemanalPlaningItem',
   props:['datos'],
-  setup(){
+  setup(props){
+    const dayOp = ref(['Lunes','Martes','Miercoles','Jueves'])
+    const dia = ref(props.datos.dia)
+    const actividad = ref(props.datos.actividad)
     const actActividad = ref(false)
-    const dayOld = ref('')
-    const activityOld = ref('')
-
+    //prueba
+    function open () {
+      actActividad.value = true
+    }
     //Eliminar actividad
     function borrar(id) {
       api.delete(`planning/semanal/${id}`)
         .then(res => {
           console.log(res.data)
-          this.$emit('reload')
         })
     }
     //Actualizar actividad
-    function actualizar(id) {
-      api.patch(`planning/semanal/${id}`)
+    function actualizar(id,dia,actividad) {
+      api.patch(`planning/semanal/${id}`,{day:dia,activity:actividad})
         .then(res => {
           console.log(res.data)
+          actActividad.value = false
           this.$emit('reload')
         })
     }
     return{
+      dayOp,
+      dia,
+      actividad,
       actActividad,
-      activityOld,
-      dayOld,
       borrar,
-      actualizar
+      actualizar,
+      open
     }
   }
 })
